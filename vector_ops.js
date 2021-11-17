bda5ed6a63fb88e35a9e70e67f684dacd68c198d
Rishav Chatterjee <chat.rishav7@gmail.com>
2021-11-17 14:48:52 +0000

    vector ops edit

--- vector_ops.js
+++ vector_ops.js
@@ -1,48 +1,43 @@
 const Vector = Object.create(null);
 
-Vector.crossProduct = function (vector1, vector2) {
+Vector.crossProduct = function ([x1, y1, z1], [x2, y2, z2]) {
     return [
-        vector1[1] * vector2[2] - vector1[2] * vector2[1],
-        vector1[2] * vector2[0] - vector1[0] * vector2[2],
-        vector1[0] * vector2[1] - vector1[1] * vector2[0]
+        y1 * z2 - z1 * y2,
+        z1 * x2 - x1 * z2,
+        x1 * y2 - y1 * x2
     ];
 };
 
-Vector.dotProduct = function (vector1, vector2) {
-    return vector1[0] * vector2[0] + vector1[1] * vector2[1] +
-    vector1[2] * vector2[2];
+Vector.dotProduct = function ([x1, y1, z1], [x2, y2, z2]) {
+    return x1 * x2 + y1 * y2 + z1 * z2;
 };
 
-Vector.addVector = function (vector1, vector2, vector3) {
+Vector.addVector = function ([x1, y1, z1], [x2, y2, z2], vector3) {
     if (vector3 === undefined) {
         vector3 = [0, 0, 0];
     }
     return [
-        vector1[0] + vector2[0] + vector3[0],
-        vector1[1] + vector2[1] + vector3[1],
-        vector1[2] + vector2[2] + vector3[2]
+        x1 + x2 + vector3[0],
+        y1 + y2 + vector3[1],
+        z1 + z2 + vector3[2]
     ];
 };
 
-Vector.subVector = function (vector1, vector2) {
-    return [
-        vector1[0] - vector2[0],
-        vector1[1] - vector2[1],
-        vector1[2] - vector2[2]
-    ];
+Vector.subVector = function ([x1, y1, z1], [x2, y2, z2]) {
+    return [x1 - x2, y1 - y2, z1 - z2];
 };
 
-Vector.scalarMult = function (vector, s) {
-    return [vector[0] * s, vector[1] * s, vector[2] * s];
+Vector.scalarMult = function ([x, y, z], s) {
+    return [x * s, y * s, z * s];
 };
 
-Vector.negVector = function ([x, y, z]) {
+Vector.negate = function ([x, y, z]) {
     return [-x, -y, -z];
 };
 
-Vector.normalise = function (vector) {
-    let mag = Math.hypot(...vector);
-    return [vector[0] / mag, vector[1] / mag, vector[2] / mag];
+Vector.normalise = function ([x, y, z]) {
+    let mag = Math.hypot(x, y, z);
+    return [x / mag, y / mag, z / mag];
 };
 
 export default Object.freeze(Vector);
